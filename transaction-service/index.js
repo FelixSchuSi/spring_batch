4fb4cf365d86654ecf170976be967e7f4d23b5ad
Felix Schulze Sindern <felixs@fh-muenster.de>
2020-12-27 15:59:09 +0100

    Copied REST API code from ohter project

---
+++ transaction-service/index.js
@@ -0,0 +1,76 @@
+import express from "express";
+import * as bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import * as jwt from "jsonwebtoken";
+import * as http from "http";
+import tasks from "./routes/tasks";
+import users from "./routes/users";
+import meals from "./routes/meals";
+import startDB from "./db";
+
+const port = 3443;
+
+function configureApp(app) {
+  app.get("/", (req, res) => {
+    const healthCheckMessage = "health check successfull!";
+    console.log(success);
+    res.send(success);
+  });
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(cookieParser());
+  app.use((req, res, next) => {
+    if (isOriginAllowed(req.get("Origin"))) {
+      res.set("Access-Control-Allow-Origin", req.get("Origin"));
+      res.set("Access-Control-Allow-Credentials", "true");
+    }
+    if (isPreflight(req)) {
+      res.set("Access-Control-Allow-Headers", "Content-Type");
+      res.set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+      res.status(204).end();
+    } else {
+      next();
+    }
+  });
+  app.use("/api/meals", meals);
+  app.use("/api/users", users);
+  app.use((req, res, next) => {
+    const token = req.cookies["jwt-token"] || "";
+    try {
+      res.locals.user = jwt.verify(token, "mysecret");
+      next();
+    } catch (error) {
+      res.status(401).json({ message: "Bitte melden Sie sich an!" });
+    }
+  });
+  app.use("/api/tasks", tasks);
+}
+
+function isPreflight(req) {
+  return (
+    req.method === "OPTIONS" &&
+    req.get("Origin") &&
+    req.get("Access-Control-Request-Method")
+  );
+}
+
+function isOriginAllowed(origin) {
+  return !!origin;
+}
+
+async function start() {
+  const app = express();
+
+  configureApp(app);
+  await startDB(app, process.argv[2]);
+  startHttpServer(app);
+}
+
+function startHttpServer(app) {
+  const httpServer = http.createServer(app);
+  httpServer.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+  });
+}
+
+start();
