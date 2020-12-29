f891560310e25a0714e3837ed12f7d6904cfc584
Felix Schulze Sindern <fs988544@fh-muenster.de>
2020-12-29 18:17:14 +0100

    Server now returns random 500s

--- transaction-service/index.js
+++ transaction-service/index.js
@@ -2,22 +2,38 @@ const express = require("express");
 const http = require("http");
 const { startDB } = require("./db.js");
 
+const requestMap = new Map();
 const port = 3443;
 
 function configureApp(app) {
   app.get("/", (req, res) => {
-    const healthCheckMessage = "health check successfull!";
-    console.log(healthCheckMessage);
-    res.send(healthCheckMessage);
+    const HEALTHMSG = "health check successfull!";
+    console.log(HEALTHMSG);
+    res.send(HEALTHMSG);
   });
   app.get("/transactions", async (req, res) => {
     const accountId = Number(req.query.accountId);
     if (Number.isNaN(accountId)) { res.sendStatus(400); return }
-
+    const return500 = Math.random() > 0.5;
+    if (return500 && requestMap.has(accountId) && requestMap.get(accountId) + 1 < 3) {
+      const numberOfRequestsForId = requestMap.get(accountId) + 1;
+      console.log(`${numberOfRequestsForId} time for acc ${accountId} - Server error`);
+      requestMap.set(accountId, numberOfRequestsForId);
+      res.sendStatus(500);
+      return;
+    } else if (return500 && !requestMap.has(accountId)) {
+      console.log(`First time for acc ${accountId} - Server error`);
+      requestMap.set(accountId, 1);
+      res.sendStatus(500);
+      return;
+    } else if (requestMap.has(accountId) && requestMap.get(accountId) + 1 >= 3) {
+      console.log(`Third time for acc ${accountId} - request successful`);
+      requestMap.delete(accountId);
+    } else {
+      console.log(`acc ${accountId} - request successful`);
+    }
     const { transactionDAO } = req.app.locals;
     const transactions = await transactionDAO.findAll({ accountId });
-
-    if (accountId === 99) console.log(`request for accountId: ${accountId} returning ${transactions.length} transactions.`);
 
     res.json(transactions);
   });
