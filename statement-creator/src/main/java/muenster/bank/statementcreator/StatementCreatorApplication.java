3a32c4733d343c1c3cd03619fb50df2a296e56d7
Felix Schulze Sindern <fs988544@fh-muenster.de>
2021-01-20 10:42:42 +0100

    unnoeting imports entfernen

--- statement-creator/src/main/java/muenster/bank/statementcreator/StatementCreatorApplication.java
+++ statement-creator/src/main/java/muenster/bank/statementcreator/StatementCreatorApplication.java
@@ -1,7 +1,5 @@
 package muenster.bank.statementcreator;
 
-import javax.swing.Spring;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
